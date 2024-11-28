9f25c13b8a46b8b08438fd578a072f0447778d97
geovannad <geovanna.diniz@germinare.org.br>
2024-11-28 18:35:38 -0300

    initial settings

---
+++ src/main/java/com/example/msinternauticacrm/MsInternauticaCrmApplication.java
@@ -0,0 +1,13 @@
+package com.example.msinternauticacrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsInternauticaCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsInternauticaCrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/msinternauticacrm/MsInternauticaCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msinternauticacrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsInternauticaCrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
