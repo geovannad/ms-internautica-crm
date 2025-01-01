b91694848f05155652576710b5b798f150898155
geovannad <124395883+geovannad@users.noreply.github.com>
2025-01-01 20:33:35 -0300

    Update Announcement.java

--- src/main/java/com/example/msinternauticacrm/models/Announcement.java
+++ src/main/java/com/example/msinternauticacrm/models/Announcement.java
@@ -12,15 +12,12 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @NoArgsConstructor
 @Embeddable
 public class Announcement {
-    @NotNull(message = "Announce site status is required.")
+    
     private Boolean announceSite;
 
-    @NotNull(message = "Announce Mercado Livre status is required.")
     private Boolean announceMercadoLivre;
 
-    @NotNull(message = "Announce Bombarco status is required.")
     private Boolean announceBombarco;
 
-    @NotNull(message = "Announce Instagram status is required.")
     private Boolean announceInstagram;
 }
