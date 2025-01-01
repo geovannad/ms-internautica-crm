c831e97773a7a47474ab652bc4bb20958031cd1d
geovannad <124395883+geovannad@users.noreply.github.com>
2025-01-01 20:33:01 -0300

    Update Owner.java

--- src/main/java/com/example/msinternauticacrm/models/Owner.java
+++ src/main/java/com/example/msinternauticacrm/models/Owner.java
@@ -2,7 +2,6 @@ package com.example.msinternauticacrm.models;
 
 import jakarta.persistence.Embeddable;
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -13,10 +12,9 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @NoArgsConstructor
 @Embeddable
 public class Owner {
-    @NotBlank(message = "Owner name is required.")
+    
     private String name;
 
-    @NotBlank(message = "Owner number is required.")
     private String number;
 
     @Email(message = "Owner email must be valid.")
